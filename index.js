90f98b0fc05cf8edadcbd4e5532721153fd30cbd
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-27 18:33:44 -0400

    Create observables from static values using of.

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-import { fromEvent } from 'rxjs';
-
-const source$ = fromEvent(document, 'click');
+import { of, rage, range } from 'rxjs';
 
 const observer = {
-  next: (val) => {
-    console.log('next', val);
-  },
-  error: (val) => {
-    console.log('error', error);
-  },
-  complete: (val) => {
-    console.log('complete');
-  },
+  next: (val) => console.log('next', val),
+  error: (val) => console.log('error', error),
+  complete: (val) => console.log('complete'),
 };
 
-const sub1 = source$.subscribe(observer);
-const sub2 = source$.subscribe(observer);
+/*
+ * Emits each item you provide in sequence, synchronously.
+ * of literally just loops through the items and emits them,
+ * there is no flattening involved. For instance, if you pass an
+ * array the entire array will be emitted, not each item within
+ * the array.
+ */
 
-setTimeout(() => {
-  console.log('unsubscribing');
-  sub1.unsubscribe();
-  // sub2 still subscribed to the click event.
-}, 3000);
+const source$ = of(1, 2, 3, 4, 5, 6);
+const source2$ = of([1, 2], [3, 4], [5, 6]);
+const source3$ = of([1, 2, 3, 4, 5, 6]);
+const source4$ = of('hello');
+const source5$ = range(1, 6);
+
+console.log('proving');
+source$.subscribe(observer); // 1, 2, 3, 4, 5, 6
+// source2$.subscribe(observer); // [1, 2], [3, 4], [5, 6]
+// source3$.subscribe(observer); // // [1, 2, 3, 4, 5, 6]
+// source4$.subscribe(observer); // hello
+source5$.subscribe(observer); // 1, 2, 3, 4, 5, 6
+console.log('this is synchronous');
