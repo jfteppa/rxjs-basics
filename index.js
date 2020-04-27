87d86d6678bd06c03c44a887560d08e75cd2a515
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-27 19:27:48 -0400

    Emit items based on a duration with interval and timer.

--- index.js
+++ index.js
@@ -1,56 +1,33 @@
-import { range, from } from 'rxjs';
-import 'regenerator-runtime/runtime';
-
-const observer = {
-  next: (val) => console.log('next', val),
-  error: (error) => console.log('error', error),
-  complete: () => console.log('complete'),
-};
+import { interval, timer } from 'rxjs';
 
 /*
- * from can turn nearly anything into an observable
- * When from receieves an array, it loops through each item
- * within that array, emitting them in sequence.
+ * interval emits numbers in sequence based on the
+ * duration that you specify. In this case, a number
+ * will be emitted every 1000ms (1s)
  */
-//  Arrays
-const source_$ = from([1, 2, 3, 4, 5, 6]);
-const source$ = from(range(1, 6));
-source_$.subscribe(observer); // 1, 2, 3, 4, 5, 6
-source$.subscribe(observer); // 1, 2, 3, 4, 5, 6
 
 /*
- * This works for any array like object as well, for instance,
- * when from receieves a string (which has a length property)
- * it will loop through emitting each character.
+ * We'll just supply a function for next in this case,
+ * rather than observer object.
+ *
+ * Remember if you onle provide a single element to subscribe,
+ * that function is register as the 'next' callback
  */
-// Strings
-const source2$ = from('Hello');
-// source2$.subscribe(observer); // H, e, l , l, o
+const interval$ = interval(1000);
+// interval$.subscribe(console.log);
 
 /*
- * When from receieves a promise, it will call .then, emitting
- * the response. We will see ways to make requests using an
- * observable interface in upcoming lessons, but for now we will
- * just use fetch.
+ * If you need the first item to be emitted on an interval
+ * different than the rest, you can use the timer operator instead.
+ * For example, let's have the first item emit after 2 seconds,
+ * followed and the others every 1 seconfs.
  */
-// Promises
-const source3$ = from(fetch('https://api.github.com/users/octocat'));
-source3$.subscribe(observer);
-// next gets the response
+const timer$ = timer(2000, 1000);
+// timer$.subscribe(console.log);
 
 /*
- * When from receieves a iterator it will drop it in a do while loop,
- * calling .next and emitting each item until there are no more items left.
+ * You can also emit a single item after a specified duration, then complete,
+ * by just supplying the first argument.
  */
-
-// Iterators
-function* hello() {
-  yield 'Hello';
-  yield 'World';
-}
-
-const iterator = hello();
-// console.log(iterator.next().value); // Hello
-// console.log(iterator.next().value); // World
-const source4$ = from(iterator);
-// source4$.subscribe(observer);
+const timer2$ = timer(2000);
+timer2$.subscribe(console.log);
