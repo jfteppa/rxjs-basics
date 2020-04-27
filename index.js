23c8bbfbffa55f027949a2f5501a07cddfe53e9d
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-27 17:09:23 -0400

    Create your first observable.

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-import { of } from 'rxjs';
+import { Observable } from 'rxjs';
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
+// const observable = Observable.create // static function does the same as new Observable.
+
+/**
+ * the constructor takes a function suscriber that receives a subscriber.
+ * this subscriber is an object that handles 3 callbacks (next, error and complete).
+ * next can be called several times until complete gets called.
+ * after the complete callback gets called next and error won't be call anymore
+ * the error callback is called at most 1 time on completion.
  */
-of('Hello', 'RxJS').subscribe(console.log);
+const observable = new Observable((subscriber) => {
+  subscriber.next('Hello');
+  subscriber.next('Wrold');
+  subscriber.complete();
+  subscriber.next('Hello');
+  subscriber.next('Wrold');
+});
+
+const observer = {
+  next: (value) => console.log('next', value),
+  error: (error) => console.log('error', error),
+  complete: () => console.log('complete!'),
+};
+
+/**
+ * in order to hook up an observer to an observable
+ * beginning an observable execution you must subscribe.
+ */
+observable.subscribe(observer);
