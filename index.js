7f28c1e0274e2fbd2cd1a8dff9a717e3cacbabab
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-27 17:50:21 -0400

    Manage observable subscriptions with unsubscribe.

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
 import { Observable } from 'rxjs';
 
-const observable = new Observable((subscriber) => {
-  subscriber.next('Hello');
-  subscriber.next('Wrold');
-  subscriber.complete();
-});
-
 const observer = {
   next: (value) => console.log('next', value),
   error: (error) => console.log('next', error),
   complete: () => console.log('complete!!!'),
 };
 
-// synchronously
-console.log('before');
-observable.subscribe(observer);
-console.log('after');
-
-// asynchronously
-const observable2 = new Observable((subscriber) => {
+const observable = new Observable((subscriber) => {
   let count = 0;
 
   const id = setInterval(() => {
     subscriber.next(count);
-    subscriber.complete();
     count += 1;
   }, 1000);
 
-  // we can return a function when the observable completes.
   return () => {
+    console.log('id', id);
     console.log('calling clearInterval');
     clearInterval(id);
   };
 });
 
-observable2.subscribe(observer);
+const subscription = observable.subscribe(observer);
+
+/* setTimeout(() => {
+  subscription.unsubscribe();
+}, 3500); */
+
+/**
+ * Note: Calling unsubscribe will not fire your complete callback,
+ * but it will call the returned function.
+ */
+
+const subscription2 = observable.subscribe(observer);
+
+/*
+ * Subscriptions can be added together using the add method,
+ * you can then unsubscribe to multiple at the same time.
+ * This is simply personal preference, unsubscribing individually
+ * will produce the same result. Also, in future lessons, we will see how
+ * to automate this unsubscribe process with operators.
+ */
+
+subscription.add(subscription2);
+
+setTimeout(() => {
+  // unsubscribing individually
+  /* subscription.unsubscribe();
+  subscription2.unsubscribe(); */
+
+  // unsubscribe to multiple at the same time
+  subscription.unsubscribe();
+}, 3500);
