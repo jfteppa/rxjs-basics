4fc282d88fa912d8fab022d63b705b10b0db06f4
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-28 16:08:39 -0400

    Complete a stream when a condition is met with takeWhile.

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-import { of, fromEvent } from 'rxjs';
-import { take, map, first, filter } from 'rxjs/operators';
+import { fromEvent, interval } from 'rxjs';
+import { scan, map, mapTo, takeWhile, tap } from 'rxjs/operators';
 
-const numbers$ = of(1, 2, 3, 4, 5);
-
-// numbers$.subscribe(console.log);
+/**
+ * BEGIN FIRST SECTION OF LESSON
+ */
+const click$ = fromEvent(document, 'click');
 
 /*
- * take emits the first x values from the source,
- * then completes. In this case, 1,2,3 will be emitted.
+ * takeWhile emits values as long as they pass
+ * the provided condition. As soon as the predicate
+ * returns false, takeWhile completes the observable.
+ * You can also pass an optional second parameter of true
+ * if you want takeWhile to emit the value that caused
+ * your condition to return false, before completing.
  */
-numbers$.pipe(take(3)).subscribe({
-  next: console.log,
-  complete: () => console.log('Complete!'),
-});
-
-const click$ = fromEvent(document, 'click');
 
 /* click$
   .pipe(
@@ -22,32 +21,49 @@ const click$ = fromEvent(document, 'click');
       x: event.clientX,
       y: event.clientY,
     })),
-    take(1)
+    // takes until condition is met and DOES NOT RETURN VALUE MEETING CONDITION
+    // takeWhile(({ y }) => y <= 200)
+    // RETURNS VALUE WHEN CONDITION IS MET
+    takeWhile(({ y }) => y <= 200, true)
   )
   .subscribe({
     next: console.log,
     complete: () => console.log('Complete!'),
   }); */
 
-/*
- * In this example, we will take the first value that matches
- * the provided criteria before completing. While we could use
- * a combination of filter(condition) and take(1), we can also
- * use the first operator to fulfill the same use case.
- * If you supply no values to first, it is equivalent to take(1).
+/**
+ * BEGIN SECOND SECTION OF LESSON
  */
 
-click$
+// elem refs
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+
+// streams
+const counter$ = interval(1000);
+
+counter$
   .pipe(
-    map((value) => ({
-      x: event.clientX,
-      y: event.clientY,
-    })),
-    // filter(({ y }) => y > 200), // filters everything to be y > 200
-    // take(1) // takes 1 and completes
-    first(({ y }) => y > 200) // filter and take in 1 operator
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 10),
+    // proving the interval stops
+    tap(console.log),
+    /*
+     * Instead of filter let's use takeWhile. This will
+     * complete the observable and clean up the interval
+     * once the countdown goes below zero, rather than
+     * just preventing the numbers from being emitted.
+     */
+    takeWhile((value) => value >= 0)
   )
   .subscribe({
-    next: console.log,
+    next: (value) => {
+      countdown.innerHTML = value;
+      if (!value) {
+        message.innerHTML = 'Liftoff!';
+      }
+    },
     complete: () => console.log('Complete!'),
   });
