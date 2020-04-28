1507016d06d7ad9b7cdb8a63597146142e5177f5
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-28 00:12:56 -0400

    Debug your observable streams with tap.

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
-import { interval } from 'rxjs';
-import { scan, mapTo, filter, take } from 'rxjs/operators';
+import { of, interval } from 'rxjs';
+import { map, tap, mapTo, scan, filter } from 'rxjs/operators';
+
+const numbers$ = of(1, 2, 3, 4, 5);
+
+/*
+ * tap can be used to spy on your streams, performing side effects
+ * such as logging, and is particularly useful for debugging.
+ * In the example below, we are spying on the value before and after
+ * the map operator.
+ */
+
+/*
+ * tap also accepts an observer object, if you wish to also
+ * receieve notifications on complete or error. You will use this
+ * far less often, but it's good to know just in case...
+ */
+
+numbers$
+  .pipe(
+    // tap((value) => console.log('before', value)),
+    tap((value) => {
+      // console.log('before', value);
+      // tap completely ignores any return value from the function you provide
+      return 100;
+    }),
+    map((value) => value * 10),
+    // tap((value) => console.log('after', value))
+    tap({
+      // next: (value) => console.log('after', value),
+      // complete: () => console.log('done!'),
+      error: (error) => {
+        // do something
+      },
+    })
+  )
+  .subscribe((value) => {
+    // console.log('from next callback', value);
+  });
 
 // elem refs
 const countdown = document.getElementById('countdown');
 const message = document.getElementById('message');
 
 // streams
-const counter$ = interval(1000);
+/* const counter$ = interval(1000);
 
-/* counter$
+counter$
   .pipe(
     // for each value emitted we map it to -1
     mapTo(-1),
     scan((accumulator, currentValue) => {
       return accumulator + currentValue;
     }, 10),
+    tap(console.log),
     filter((value) => value >= 0)
   )
   .subscribe((value) => {
@@ -23,28 +61,3 @@ const counter$ = interval(1000);
       message.innerHTML = 'Liftoff!';
     }
   }); */
-
-/**
- * with the example from above the interval never stops
- * with this new example it completes wen it takes 10 numbers
- */
-
-counter$
-  .pipe(
-    mapTo(-1),
-    scan((a, c) => {
-      console.log({ a, c });
-      return a + c;
-    }, 10),
-    take(10)
-  )
-  .subscribe({
-    next: (value) => {
-      console.log(value);
-      countdown.innerHTML = value;
-    },
-    complete: () => {
-      console.log('complete');
-      message.innerHTML = 'Liftoff!';
-    },
-  });
