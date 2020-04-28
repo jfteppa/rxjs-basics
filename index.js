a833a031f09d73a1afd52e501989298020f2f565
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-27 23:11:50 -0400

    Manage state changes incrementally with scan.

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
 import { from, interval } from 'rxjs';
-import { reduce, take } from 'rxjs/operators';
+import { reduce, scan, map, pluck } from 'rxjs/operators';
 
 const numbers = [1, 2, 3, 4, 5];
 
-/*
- * Reducer functions take the accumulated value (last return)
- * and current value, returning a new accumulated value.
- * You can think of this like a snowball rolling downhill,
- * collecting values over time.
- */
-const totalReducer = (accumulator, currentValue) => {
-  console.log({ accumulator, currentValue });
-  return accumulator + currentValue;
-};
-
-const total = numbers.reduce(totalReducer, 0);
-console.log(total);
+from(numbers)
+  .pipe(reduce((accumulator, currentValue) => accumulator + currentValue))
+  .subscribe(console.log);
 
 /*
- * Our reducer function is invoked on each emission and the accumulated
- * value stored. On completion the current accumulated value is emitted.
- * In this example we are supplying a seed value (initial accumulator) of 0
- * as the second parameter.
+ * scan is similar to reduce, except it emits each new acculumated
+ * value as it occurs. This is great for managing state changes
+ * in your application over time.
  */
-from(numbers).pipe(reduce(totalReducer, 0)).subscribe(console.log);
+from(numbers)
+  .pipe(scan((accumulator, currentValue) => accumulator + currentValue))
+  .subscribe(console.log);
 
-from(numbers).pipe(take(3), reduce(totalReducer, 0)).subscribe(console.log);
+const user = [
+  { name: 'Brian', loggedIn: false, token: null },
+  { name: 'Brian', loggedIn: true, token: 'abc' },
+  { name: 'Brian', loggedIn: true, token: '123' },
+];
 
-interval(2000)
-  .pipe(take(3), reduce(totalReducer, 0))
-  .subscribe({
-    next: console.log,
-    complete: () => console.log('complete!!!'),
-  });
+const state$ = from(user).pipe(
+  scan((accumulator, currentValue) => {
+    return { ...accumulator, ...currentValue };
+  }, {})
+);
 
-/*
- * Important! reduce only emits one value, the final accumulated value
- * on completion. We are forcing completion by using the take operator.
- * If you want to emit each new accumulated value, you will use the scan
- * operator, which is the focus of the next lesson.
- */
+const names$ = state$.pipe(map((state) => state.name));
+names$.subscribe(console.log);
+
+// same but less code.
+const names2$ = state$.pipe(pluck('name'));
+names2$.subscribe(console.log);
