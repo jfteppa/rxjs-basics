a49964fb9d2ada0fea8056c6031a302bf3d99212
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-27 22:49:04 -0400

    Accumulate data over time using reduce.

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { from, interval } from 'rxjs';
+import { reduce, take } from 'rxjs/operators';
+
+const numbers = [1, 2, 3, 4, 5];
 
 /*
- * Calculate progress based on scroll position
+ * Reducer functions take the accumulated value (last return)
+ * and current value, returning a new accumulated value.
+ * You can think of this like a snowball rolling downhill,
+ * collecting values over time.
  */
-function calculateScrollPercent(element) {
-  const { scrollTop, scrollHeight, clientHeight } = element;
-  return (scrollTop / (scrollHeight - clientHeight)) * 100;
-}
+const totalReducer = (accumulator, currentValue) => {
+  console.log({ accumulator, currentValue });
+  return accumulator + currentValue;
+};
 
-// elems
-const progressBar = document.querySelector('.progress-bar');
-
-// streams
-const scroll$ = fromEvent(document, 'scroll');
-
-const progress$ = scroll$.pipe(
-  map(({ target }) => calculateScrollPercent(target.scrollingElement))
-);
+const total = numbers.reduce(totalReducer, 0);
+console.log(total);
 
 /*
- * We can then take the emitted percent and set the width
- * on our progress bar.
+ * Our reducer function is invoked on each emission and the accumulated
+ * value stored. On completion the current accumulated value is emitted.
+ * In this example we are supplying a seed value (initial accumulator) of 0
+ * as the second parameter.
  */
-progress$.subscribe((percent) => {
-  progressBar.style.width = `${percent}%`;
-  console.log(percent);
-});
+from(numbers).pipe(reduce(totalReducer, 0)).subscribe(console.log);
+
+from(numbers).pipe(take(3), reduce(totalReducer, 0)).subscribe(console.log);
+
+interval(2000)
+  .pipe(take(3), reduce(totalReducer, 0))
+  .subscribe({
+    next: console.log,
+    complete: () => console.log('complete!!!'),
+  });
+
+/*
+ * Important! reduce only emits one value, the final accumulated value
+ * on completion. We are forcing completion by using the take operator.
+ * If you want to emit each new accumulated value, you will use the scan
+ * operator, which is the focus of the next lesson.
+ */
