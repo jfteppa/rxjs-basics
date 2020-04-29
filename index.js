3edcab2ff8be8d09a3201f18b4e3e035f1c370cd
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-29 15:51:10 -0400

    Switch to a new observable on emissions using switchMap.

--- index.js
+++ index.js
@@ -1,68 +1,73 @@
-import { fromEvent, interval } from 'rxjs';
+import { fromEvent, empty, interval } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { mergeMap, catchError, takeUntil, map } from 'rxjs/operators';
+import {
+  debounceTime,
+  pluck,
+  distinctUntilChanged,
+  switchMap,
+  catchError,
+  mergeMap,
+} from 'rxjs/operators';
 
 /*
  * BEGIN FIRST SECTION
  */
-const click$ = fromEvent(document, 'click');
 const interval$ = interval(1000);
+const click$ = fromEvent(document, 'click');
 
-// click$.pipe(
-/*
- * mergeMap invokes the function you provide,
- * subscribing to each returned observable internally.
- * Any values emitted by these inner observables
- * are then emitted by mergeMap. By default there
- * is no limit to the number of active inner
- * subscriptions that can be active at a time
- * with mergeMap, so if you continually click on
- * the page more and more timers will be activated.
- * This can be dangerous if you have long
- * running inner observables and forget to clean
- * them up.
+/**
+ * for each click it initiates another interval and does not cancel previous
  */
-//   mergeMap(() => interval$)
-// ).subscribe(console.log);
+// click$.pipe(mergeMap(() => interval$)).subscribe(console.log);
+
+const clickInstance = click$.pipe(
+  /*
+   * switchMap switches to a new observable on each emission
+   * from the source, cancelling any previous inner
+   * observables. For instance, if you click once a new
+   * interval observable will be subscribed to internally,
+   * with it's values emitted. When you click again,
+   * that observable will be completed, and the next
+   * interval will be subscribed to, restarting
+   * the count. This will happen on each emission from
+   * the click$ observable.
+   */
+  switchMap(() => interval$)
+);
+
+// clickInstance.subscribe(console.log);
 
 /*
  * BEGIN SECOND SECTION
  */
-const mousedown$ = fromEvent(document, 'mousedown');
-const mouseup$ = fromEvent(document, 'mouseup');
+const BASE_URL = 'https://api.openbrewerydb.org/breweries';
 
-mousedown$
+//elems
+const inputBox = document.getElementById('text-input');
+const typeaheadContainer = document.getElementById('typeahead-container');
+
+// streams
+const input$ = fromEvent(inputBox, 'keyup');
+
+input$
   .pipe(
+    // to test, comment out the debounceTime and go to the network tab
+    debounceTime(200),
+    pluck('target', 'value'),
+    distinctUntilChanged(),
+    // mergeMap((searchTerm) => ajax.getJSON(`${BASE_URL}?by_name=${searchTerm}`))
     /*
-     * In this case, we are mapping to a new interval
-     * observable on mousedown, but we are limiting it's
-     * lifetime by using the takeUntil operator with
-     * the mouseup$ stream.
+     * switchMap is perfect for GET requests, as you do
+     * not normally care about the previous request
+     * to the same URL if another has fired. For instance,
+     * in this example if the user continues typing
+     * and the previuos request has not returned,
+     * switchMap will go ahead and cancel it and only
+     * the current request will be considered.
      */
-    mergeMap(() => interval$.pipe(takeUntil(mouseup$)))
+    switchMap((searchTerm) => ajax.getJSON(`${BASE_URL}?by_name=${searchTerm}`))
   )
-  .subscribe(console.log);
-
-/*
- * BEGIN THIRD SECTION
- */
-const coordinates$ = click$.pipe(
-  map((event) => ({
-    x: event.clientX,
-    y: event.clientY,
-  }))
-);
-
-const coordinatesWithSave$ = coordinates$.pipe(
-  /*
-   * mergeMap is good for 'fire and forget' save request
-   * you do not want to be cancelled. For instance, in this
-   * example we are emulating a save of coordinates
-   * anytime the user clicks on the page.
-   */
-  mergeMap((coords) =>
-    ajax.post('https://www.mocky.io/v2/5185415ba171ea3a00704eed', coords)
-  )
-);
-
-coordinatesWithSave$.subscribe(console.log);
+  .subscribe((response) => {
+    // update ui
+    typeaheadContainer.innerHTML = response.map((b) => b.name).join('<br>');
+  });
